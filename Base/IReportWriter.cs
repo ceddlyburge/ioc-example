eb9f55b52170e593a8b9a8cb509ba32958677471
Clément Bouscasse <clement.bouscasse@res-ltd.com>
2015-06-05 17:58:16 +0100

    initial commit

---
+++ Base/IReportWriter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reporting.Base
+{
+    /*
+     In the code review meeting we talked about adding interfaces like this, probably both to be implemented by the same classes as currently, because the ReportWriter wants these things to be called, but the Report's don't want to have any knowledge of them
+     
+
+    // this to be passed to Reports and has everything they need
+    public interface IReportWriter
+    {
+        void Heading(string heading);
+        void UnorderedList(params string[] unorderedList);
+    }
+
+    // this used by the ui to allow user to choose which writer to use and possibly to do some setup with the mimetype (as in the mvc application)
+    public interface IReportWriterIdentification // maybe : IReportWriter
+    {
+        string Name { get; }
+        string MimeType { get; }
+    }
+
+    // this has things that the ReportWriter's want called
+    public interface IReportWriterInternalWorkings  // maybe : IReportWriter or IReportWriterIdentification
+    {
+        // these are a code smell as they mean that the interface has some knowledge of the class(es) that will implement them. In this case I couldn't really think of another way to do this and it seems that a reasonable proportion of conceivable report writers would want / need these, so I don't think its too bad. If just one implementation needed it then it would be bad.
+        // Another option for begin is for the implemention to call this itself the first time a writing method is called. This would only work if the ioc creates a new instance for every request, which is a bit less efficient but not really a big deal.
+        void Begin(); 
+        void End();
+    }
+    */
+    public interface IReportWriter
+    {
+        string Name { get; }
+        string MimeType { get; }
+
+        void Begin();
+        void End();
+
+        // these could have guards to make sure Begin has been called
+        void Heading(string heading);
+        void UnorderedList(params string [] unorderedList);
+        // plus stuff for tables and graphs and all that
+    }
+}
---
+++ Base/Report.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Reporting.Base
+{
+    public abstract class Report
+    {
+        public abstract string Name { get; }
+        // this modifier means protected *or* internal. It means that reports have access to override it and that the ReportGenerator (in this assembly) can call it. But nothing else has access, which means that the ReportGenerator has to be used to make reports. Which guarantees that the Begin and End methods of IReportWriter are called. Otherwise this abstract class could instead be an interface. This wouldn't make a conceptual difference but does mean that each Report could inherit from something else. We are meant to favour composition over inheritance these days though so this shouldn't be an issue.
+        protected internal abstract void Generate(IReportWriter write);
+    }
+}
